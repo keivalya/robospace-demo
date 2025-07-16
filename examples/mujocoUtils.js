935408bb3229cd1076914308f72749e912b456fe
keivalya <keivalyapandya2001@gmail.com>
2025-07-16 02:05:26 -0400

    added a1 and go1 models

--- examples/mujocoUtils.js
+++ examples/mujocoUtils.js
@@ -52,9 +52,9 @@ export function setupGUI(parentContext) {
   // Add scene selection dropdown.
   let reload = parentContext.reloadFunc.bind(parentContext);
   parentContext.gui.add(parentContext.params, 'scene', {
-    // "Unitree A1": "unitree_a1/scene.xml",
     "Unitree H1": "unitree_h1/scene.xml",
-    // "Unitree Go1": "unitree_go1/scene.xml",
+    "Unitree A1": "unitree_a1/scene.xml",
+    "Unitree Go1": "unitree_go1/scene.xml",
     // "Humanoid": "humanoid.xml",
     // "Ant": "ant.xml",
   }).name('Robot Model').onChange((val) => {
